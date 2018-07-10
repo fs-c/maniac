063afc452555b22606e97c40ea49bb6c8b9c8c84
lw <me@lw.ag>
2018-07-10 20:04:48 +0200

    Properly name binary.

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 cd src
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	gcc main.c beatmap.c memory.c utils.c -lX11 -lXtst -Ofast
-	mv a.out ../a.out
+	gcc main.c beatmap.c memory.c utils.c -lX11 -lXtst -Ofast -o
+	mv maniac ../maniac
 elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]]; then
-	gcc main.c beatmap.c memory.c utils.c -Ofast
-	mv a.exe ../a.exe
+	gcc main.c beatmap.c memory.c utils.c -Ofast -o maniac.exe
+	mv maniac.exe ../maniac.exe
 fi
 
 cd ..
--- devbuild.sh
+++ devbuild.sh
@@ -1,11 +1,11 @@
 cd src
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	gcc main.c beatmap.c memory.c utils.c -lX11 -lXtst -Wall -Wextra -Wpedantic
-	mv a.out ../a.out
+	gcc main.c beatmap.c memory.c utils.c -lX11 -lXtst -Wall -Wextra -Wpedantic -o maniac
+	mv maniac ../maniac
 elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]]; then
-	gcc main.c beatmap.c memory.c utils.c -Wall -Wextra -Wpedantic
-	mv a.exe ../a.exe
+	gcc main.c beatmap.c memory.c utils.c -Wall -Wextra -Wpedantic -o maniac
+	mv maniac.exe ../maniac.exe
 fi
 
 cd ..
