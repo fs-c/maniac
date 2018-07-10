00477ff5857b79f703a43c17a0a50a8356c3b526
lw <me@lw.ag>
2018-07-10 19:10:56 +0200

    Add missing declaration.

--- memory.c
+++ memory.c
@@ -16,7 +16,8 @@ ssize_t process_vm_readv(int, struct iovec *, int, struct iovec *, int, int);
 #include <tlhelp32.h>
 #endif /* ON_WINDOWS */
 
-pid_t game_process_id;
+void *time_address;
+pid_t game_proc_id;
 
 /**
  * Gets and stores the runtime of the currently playing song, internally
@@ -26,10 +27,9 @@ pid_t game_process_id;
 int32_t get_maptime(pid_t pid)
 {
 	int32_t time;
-
-#ifdef ON_LINUX
 	size_t size = sizeof(int32_t);
 
+#ifdef ON_LINUX
 	struct iovec local[1];
 	struct iovec remote[1];
 
@@ -37,14 +37,13 @@ int32_t get_maptime(pid_t pid)
 	local[0].iov_base = &time;
 
 	remote[0].iov_len = size;
-	remote[0].iov_base = (void *)time_address;
+	remote[0].iov_base = time_address;
 
-	process_vm_readv(game_process_id, local, 1, remote, 1, 0);
+	process_vm_readv(game_proc_id, local, 1, remote, 1, 0);
 #endif /* ON_LINUX */
 
 #ifdef ON_WINDOWS
-	ReadProcessMemory(game_proc, (LPCVOID)time_address, &time,
-		sizeof(int32_t), NULL);
+	ReadProcessMemory(game_proc, (LPCVOID)time_address, &time, size, NULL);
 #endif /* ON_WINDOWS */
 
 	return time;
