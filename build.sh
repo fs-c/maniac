385214a7395a42374b89a0663bb97a2168c28196
lw <me@lw.ag>
2018-07-10 19:25:46 +0200

    Update build scripts with new path.

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
+cd src
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	gcc main.c beatmap.c memory.c utils.c -lX11 -lXtst -Ofast
+	mv a.out ../a.out
 elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]]; then
 	gcc main.c beatmap.c memory.c utils.c -Ofast
+	mv a.exe ../a.exe
 fi
+
+cd ..
--- devbuild.sh
+++ devbuild.sh
@@ -1,5 +1,11 @@
+cd src
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	gcc main.c beatmap.c memory.c utils.c -lX11 -lXtst -Wall -Wextra -Wpedantic
+	mv a.out ../a.out
 elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]]; then
 	gcc main.c beatmap.c memory.c utils.c -Wall -Wextra -Wpedantic
+	mv a.exe ../a.exe
 fi
+
+cd ..
