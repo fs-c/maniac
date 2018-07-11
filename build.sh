dec6569dfae7e3d5efc9c282efa1e8efc000ed16
lw <me@lw.ag>
2018-07-11 23:09:41 +0200

    Fix build scripts for Linux.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 cd src
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	gcc main.c beatmap.c memory.c utils.c -lX11 -lXtst -Ofast -o
+	gcc main.c beatmap.c memory.c utils.c -lX11 -lXtst -Ofast -o maniac
 	mv maniac ../maniac
 elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]]; then
 	gcc main.c beatmap.c memory.c utils.c -Ofast -o maniac.exe
