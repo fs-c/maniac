b18aef8aa519efd15627d87b5ef76a54205cd76f
lw <me@lw.ag>
2018-07-11 23:11:28 +0200

    Fix options parsing and add sensible defaults.

--- scripts/find.js
+++ scripts/find.js
@@ -1,22 +1,30 @@
+const { homedir } = require('os');
 const { join, relative } = require('path');
 const { readdirSync, readFileSync, writeFileSync, existsSync } = require('fs');
 
-const config = existsSync('config.json') ? (
-    JSON.parse(readFileSync('config.json', 'utf8'))
-) : {};
+const configPath = join(__dirname, 'config.json');
+const config = existsSync(configPath) ? (
+    JSON.parse(readFileSync(configPath, 'utf8'))
+) : { notFound: true };
 
 const searchTags = process.argv[2].split(',');
-const songsPath = config.songsPath || relative(
-    process.cwd(),
-    join(require('os').homedir(), '/AppData/Local/osu!/Songs'),
-);
+const songsPath = config.songsPath ? (
+    config.songsPath.replace('~', homedir())
+) : relative(process.cwd(),join(homedir(), '/AppData/Local/osu!/Songs'));
+
+const allTrue = (arr) => {
+    return arr.reduce((ac, cu) => !cu ? ac = false : ac, true);
+}
 
 const beatmap = readdirSync(songsPath)
     .map((song) => readdirSync(join(songsPath, song))
         .map((el) => join(songsPath, song, el)))
     .reduce((ac, cu) => { ac.push(...cu); return ac; }, [])
-    .filter((el) => searchTags.map((tg) => el.includes(tg))
-        .reduce((ac, cu) => !cu ? ac = cu : ac, true))[0];
+    .filter((el) => allTrue(searchTags.map((tg) => el.includes(tg))))[0];
+
+if (!beatmap) {
+    return console.error('no matching beatmap found');
+}
 
 writeFileSync('map.osu', readFileSync(beatmap, 'utf8'), 'utf8');
 
