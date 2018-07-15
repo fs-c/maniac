5e85ef615319b1c9e8c166fdc892d3226f491e81
lw <me@lw.ag>
2018-07-15 07:46:53 +0200

    Incorporate name change.

--- devbuild.sh
+++ devbuild.sh
@@ -1,10 +1,10 @@
 cd src
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	gcc main.c beatmap.c memory.c utils.c -lX11 -lXtst -Wall -Wextra -Wpedantic -o maniac
+	gcc main.c beatmap.c process.c utils.c -lX11 -lXtst -Wall -Wextra -Wpedantic -o maniac
 	mv maniac ../maniac
 elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]]; then
-	gcc main.c beatmap.c memory.c utils.c -Wall -Wextra -Wpedantic -o maniac
+	gcc main.c beatmap.c process.c utils.c -Wall -Wextra -Wpedantic -o maniac
 	mv maniac.exe ../maniac.exe
 fi
 
