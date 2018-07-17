7dc25b672bc94840f6d5fb9c9e1035fcc06e864f
lw <me@lw.ag>
2018-07-17 09:17:35 +0200

    Fix incorrect filenames.

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 cd src
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	gcc main.c beatmap.c memory.c utils.c -lX11 -lXtst -Ofast -o maniac
+	gcc main.c beatmap.c process.c utils.c -lX11 -lXtst -Ofast -o maniac
 	mv maniac ../maniac
 elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]]; then
-	gcc main.c beatmap.c memory.c utils.c -Ofast -o maniac.exe
+	gcc main.c beatmap.c process.c utils.c -Ofast -o maniac.exe
 	mv maniac.exe ../maniac.exe
 fi
 
