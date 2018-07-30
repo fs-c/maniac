707a3d30af09485eb7d9e87d41f89f082cb7d17a
lw <me@lw.ag>
2018-07-30 04:39:44 +0200

    Add error checking and comments.

--- scripts/find.js
+++ scripts/find.js
@@ -7,6 +7,10 @@ const config = existsSync(configPath) ? (
     JSON.parse(readFileSync(configPath, 'utf8'))
 ) : { notFound: true };
 
+if (process.argv.length < 2) {
+    return console.error('error: no arguments provided');
+}
+
 const searchTags = process.argv[2].split(',');
 const songsPath = config.songsPath ? (
     config.songsPath.replace('~', homedir())
@@ -16,16 +20,23 @@ const allTrue = (arr) => {
     return arr.reduce((ac, cu) => !cu ? ac = false : ac, true);
 }
 
+if (!(existsSync(songsPath))) {
+    console.error('error: %s does not exist', songsPath);
+}
+
 const beatmap = readdirSync(songsPath)
+    // Get an array of arrays for all filenames of all song folders.
     .map((song) => readdirSync(join(songsPath, song))
         .map((el) => join(songsPath, song, el)))
+    // Reduce array of arrays to single array, we don't care about song folders.
     .reduce((ac, cu) => { ac.push(...cu); return ac; }, [])
+    // Filter out songs which don't match all tags, choose the first one.
     .filter((el) => allTrue(searchTags.map((tg) => el.includes(tg))))[0];
 
 if (!beatmap) {
-    return console.error('no matching beatmap found');
+    return console.error('error: no matching beatmap found');
 }
 
 writeFileSync('map.osu', readFileSync(beatmap, 'utf8'), 'utf8');
 
-console.log(`copied ${beatmap} to ./map.osu`);
+console.log(`copied ${beatmap} to ./map.osu`);  
