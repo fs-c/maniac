39a6cf8e6ccb58a382e871658c5b7c8f2c8c82b4
lw <me@lw.ag>
2018-08-11 13:49:38 +0200

    Add comments and notes to find_pattern.

--- src/pattern.h
+++ src/pattern.h
@@ -2,6 +2,8 @@
  * Windows only:
  * Searches for a signature (sequence of bytes) in the process, returning the
  * addresses of the end of the first occurence.
+ * This is heavily inspired by an implementation from github.com/aixxe.
+ * TODO: Use a more efficient pattern searching algorithm.
  */
 void *find_pattern(const unsigned char *signature, unsigned int sig_len)
 {
@@ -11,12 +13,15 @@ void *find_pattern(const unsigned char *signature, unsigned int sig_len)
 
 	unsigned char chunk[read_size];
 
+	// Get reasonably sized chunks of memory...
 	for (size_t i = 0; i < INT_MAX; i += read_size - sig_len) {
 		ReadProcessMemory(game_proc, (void *)i, &chunk, read_size, NULL);
 
+		// ...and iterate over them...
 		for (size_t a = 0; a < read_size; a++) {
 			hit = true;
 
+			// ...to check if they include the pattern/sig.
 			for (size_t j = 0; j < sig_len && hit; j++) {
 				if (chunk[a + j] != signature[j]) {
 					hit = false;
