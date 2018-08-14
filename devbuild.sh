91f4f5df4aaaa95a7a372a9ce7335332ce214672
lw <me@lw.ag>
2018-08-14 20:10:50 +0200

    Add DEBUG define at compile time.

--- devbuild.sh
+++ devbuild.sh
@@ -1,10 +1,10 @@
 cd src
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	gcc main.c beatmap.c process.c utils.c -lX11 -lXtst -Wall -Wextra -Wpedantic -Wno-unused-parameter -o maniac
+	gcc -DDEBUG main.c beatmap.c process.c utils.c -lX11 -lXtst -Wall -Wextra -Wpedantic -Wno-unused-parameter -o maniac
 	mv maniac ../maniac
 elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]]; then
-	gcc main.c beatmap.c process.c utils.c -Wall -Wextra -Wpedantic -o maniac
+	gcc -DDEBUG main.c beatmap.c process.c utils.c -Wall -Wextra -Wpedantic -o maniac
 	mv maniac.exe ../maniac.exe
 fi
 
