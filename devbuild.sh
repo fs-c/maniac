c9e7aa82df5a0200d0eb9795b10176f3a06eea1a
lw <me@lw.ag>
2018-08-14 13:12:23 +0200

    Don't warn about unused parameters.

--- devbuild.sh
+++ devbuild.sh
@@ -1,7 +1,7 @@
 cd src
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	gcc main.c beatmap.c process.c utils.c -lX11 -lXtst -Wall -Wextra -Wpedantic -o maniac
+	gcc main.c beatmap.c process.c utils.c -lX11 -lXtst -Wall -Wextra -Wpedantic -Wno-unused-parameter -o maniac
 	mv maniac ../maniac
 elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]]; then
 	gcc main.c beatmap.c process.c utils.c -Wall -Wextra -Wpedantic -o maniac
