11c36b34828586d68b7a521f139a43787e1f7ce2
lw <me@lw.ag>
2018-08-16 03:26:13 +0200

    Implement window name fetching on Linux, closes #3

--- src/utils.c
+++ src/utils.c
@@ -6,9 +6,17 @@
 #include <string.h>
 
 #ifdef ON_LINUX
+  Window window;
   Display *display;
 
-  #include <stdarg.h>
+  static Window find_window(pid_t pid);
+  static pid_t get_window_pid(Window window);
+  static int is_window_visible(Window window);
+  static int is_window_match(Window window, pid_t pid);
+  static void search_children(pid_t pid, Window window, Window *out);
+  static int get_window_name(Window window, char *title, int title_len);
+  static unsigned char *get_window_property(Window window, Atom atom,
+	size_t *num_items);
 #endif /* ON_LINUX */
 
 #ifdef ON_WINDOWS
@@ -63,10 +71,16 @@ void do_setup()
 {
 #ifdef ON_LINUX
 	if (!(display = XOpenDisplay(NULL))) {
-		printf("failed to open X display");
+		printf("failed to open X display\n");
 
 		return;
 	} else debug("opened X display (%#x)", display);
+
+	if (!(window = find_window(game_proc_id))) {
+		printf("failed to find game window\n");
+
+		return;
+	} else debug("found game window (%ld)", window);
 #endif /* ON_LINUX */
 
 #ifdef ON_WINDOWS
@@ -105,11 +119,16 @@ void *get_time_address()
 
 int get_window_title(char **title)
 {
-#ifdef ON_WINDOWS
 	const int title_len = 128;
 	*title = malloc(title_len);
+
+#ifdef ON_WINDOWS
 	return GetWindowText(game_window, *title, title_len);
 #endif /* ON_WINDOWS */
+
+#ifdef ON_LINUX
+	return get_window_name(window, *title, title_len);
+#endif /* ON_LINUX */
 
 	return 0;
 }
@@ -138,6 +157,152 @@ __stdcall static int enum_windows_callback(HWND handle, void *param)
 	return 0;
 }
 #endif /* ON_WINDOWS */
+
+// See above, this seems like a horrible solution.
+#ifdef ON_LINUX
+static Window find_window(pid_t pid)
+{
+	Window root = RootWindow(display, 0), found = 0;
+
+	search_children(pid, root, &found);
+
+	if (found) {
+		return found;
+	}
+
+	return 0;
+}
+
+static void search_children(pid_t pid, Window window, Window *out)
+{
+	size_t num_children = 0;
+	Window dummy, *children = NULL;
+
+	int success = XQueryTree(display, window, &dummy, &dummy, &children,
+		(unsigned *)&num_children);
+	
+	if (!success) {
+		if (children)
+			XFree(children);
+
+		printf("failed getting children of %ld\n", window);
+
+		return;
+	}
+
+	for (size_t i = 0; i < num_children; i++) {
+		Window child = children[i];
+
+		if (is_window_match(child, pid)) {
+			*out = child;			
+
+			return;
+		}
+	}
+
+	for (size_t i = 0; i < num_children; i++) {
+		search_children(pid, children[i], out);
+	}
+}
+
+static int is_window_match(Window window, pid_t pid)
+{
+	if (get_window_pid(window) != pid)
+		return 0;
+
+	if (!(is_window_visible(window)))
+		return 0;
+
+	return 1;
+}
+
+static pid_t get_window_pid(Window window)
+{
+	size_t num_items;
+	unsigned char *data;
+
+	static Atom pid_atom = -1;
+
+	if (pid_atom == (Atom)-1) {
+		pid_atom = XInternAtom(display, "_NET_WM_PID", 0);
+	}
+
+	data = get_window_property(window, pid_atom, &num_items);
+
+	pid_t pid = (num_items > 0) ? ((pid_t) *((unsigned long *)data)) : 0;
+
+	XFree(data);
+	
+	return pid;
+}
+
+static unsigned char *get_window_property(Window window, Atom atom,
+	size_t *num_items)
+{
+	Atom actual_type;
+	int actual_format;
+
+	size_t bytes_after;
+	unsigned char *prop;
+
+	int status = XGetWindowProperty(display, window, atom, 0, (~0L), 0,
+		AnyPropertyType, &actual_type, &actual_format, num_items,
+		&bytes_after, &prop);
+
+	if (status != Success) {
+		printf("failed getting window (%ld) property\n", window);
+
+		return NULL;
+	}
+
+	return prop;
+}
+
+static int is_window_visible(Window window)
+{
+	XWindowAttributes attr;
+	int success = XGetWindowAttributes(display, window, &attr);
+
+	if (!success) {
+		printf("failed getting window (%ld) attributes\n",
+			window);
+
+		return 0;
+	}
+
+	if (attr.map_state != IsViewable) {
+		return 0;
+	}
+
+	return 1;
+}
+
+static int get_window_name(Window window, char *title, int title_len)
+{
+	static Atom net_name_atom = -1, name_atom = -1;
+
+	if (name_atom == (Atom)-1)
+		name_atom = XInternAtom(display, "WM_NAME", 0);
+	if (net_name_atom == (Atom)-1)
+		net_name_atom = XInternAtom(display, "_NET_WM_NAME", 0);
+
+	 // http://standards.freedesktop.org/wm-spec/1.3/ar01s05.html
+	 // Prefer _NET_WM_NAME if available, otherwise use WM_NAME
+
+	size_t num_items = 0;
+
+	unsigned char *prop = get_window_property(window, net_name_atom,
+		&num_items);
+
+	if (!num_items) {
+		prop = get_window_property(window, name_atom, &num_items);
+	}
+
+	strcpy(title, (char *)prop);
+
+	return strlen(title);
+}
+#endif /* ON_LINUX */
 
 // TODO: I'm certain there's a more elegant way to go about this.
 int partial_match(char *base, char *partial)
@@ -199,8 +364,7 @@ void debug(char *fmt, ...)
 	va_start(args, fmt);
 
 	char message[256];
-	sprintf(message, "[debug] [t:%-6ld s:%-.8x]   ", cur_clock - old_clock,
-		(int)(uintptr_t)__builtin_return_address(1));
+	sprintf(message, "[debug] %-8ld  ", cur_clock - old_clock);
 	strcat(message, fmt);
 
 	int len = strlen(message);
