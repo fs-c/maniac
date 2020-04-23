9abbb4ed8b4984fc6bbc76f18ef0dbdc66997fa2
lw <laurenz@software-architects.at>
2020-04-24 00:39:33 +0200

    Modify devbuild to use CMake

--- devbuild.sh
+++ devbuild.sh
@@ -1,21 +1,3 @@
-CC="gcc"
-FILES="maniac.c beatmap.c process.c window.c game.c"
-
-PRE_FLAGS="-DDEBUG"
-# unused-parameter:	some functions which are stubs on some platforms would
-#			cause warnings
-# frame-address:	debug functions call unsafe functions, this is
-#			irrelevant for production and an acceptable risk here
-POST_FLAGS="-Wall -Wextra -Wpedantic -Wno-unused-parameter -Wno-frame-address -g"
-
-cd src
-
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	${CC} ${PRE_FLAGS} ${FILES} ${POST_FLAGS} -lX11 -lXtst  -o maniac
-	mv maniac ../maniac
-elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]]; then
-	${CC} ${PRE_FLAGS} ${FILES} ${POST_FLAGS} -o maniac
-	mv maniac.exe ../maniac.exe
-fi
-
-cd ..
+mkdir -p build
+cd build
+cmake .. -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_BUILD_TYPE=Debug
