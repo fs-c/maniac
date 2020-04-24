cbbba969e46e575b000f71eb2b6e29b775e36234
lw <laurenz@software-architects.at>
2020-04-24 12:58:04 +0200

    Allow passing arguments to cmake

--- devbuild.sh
+++ devbuild.sh
@@ -1,3 +1,4 @@
 mkdir -p build
 cd build
-cmake .. -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_BUILD_TYPE=Debug
+cmake .. -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_BUILD_TYPE=Debug $@
+mingw32-make
