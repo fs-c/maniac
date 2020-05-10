2309ecb44df419d8b1ef1ec883d08683a0838037
lw <laurenz@software-architects.at>
2020-05-10 23:04:44 +0200

    Generate compile_commands.json

--- devbuild.sh
+++ devbuild.sh
@@ -1,4 +1,5 @@
 mkdir -p build
 cd build
-cmake .. -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_BUILD_TYPE=Debug $@
+cmake .. -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 $@
 mingw32-make
+mv compile_commands.json ..
