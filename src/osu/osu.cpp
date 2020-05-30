9f42814e625349859cf2dd9458fea74bd22e1afd
Laurenz Weixlbaumer <root@fsoc.space>
2020-05-30 21:36:38 +0200

    Streamline address fetching

--- src/osu/osu.cpp
+++ src/osu/osu.cpp
@@ -2,25 +2,8 @@
 
 Osu::Osu() : Process("osu!.exe") {
 	// TODO: These two really want to be run in parallel.
-	auto time_address_ptr = reinterpret_cast<int32_t **>(find_pattern(TIME_SIG,
-		TIME_SIG_OFFSET));
-	auto state_address_ptr = reinterpret_cast<int32_t **>(find_pattern(STATE_SIG,
-		STATE_SIG_OFFSET));
-
-	if (!time_address_ptr) {
-		throw std::runtime_error("couldn't find time address pointer");
-	}
-
-	debug("found time address pointer at %#x", (uintptr_t)time_address_ptr);
-
-	if (!state_address_ptr) {
-		throw std::runtime_error("couldn't find state address pointer");
-	}
-
-	debug("found state address pointer at %#x", (uintptr_t)state_address_ptr);
-
-	read_memory<int32_t *>(time_address_ptr, &time_address);
-	read_memory<int32_t *>(state_address_ptr, &state_address);
+	time_address = get_address<int32_t>("time address", TIME_SIG, TIME_SIG_OFFSET);
+	state_address = get_address<int32_t>("state address", STATE_SIG, STATE_SIG_OFFSET);
 }
 
 Osu::~Osu() = default;
--- src/process/process.h
+++ src/process/process.h
@@ -27,6 +27,13 @@ public:
 	void *find_pattern(const char *pattern, int offset);
 
 	void send_keypress(int key, bool down);
+
+	/**
+	 * Assumes that find_pattern(sig, off) is a pointer to a pointer containing
+	 * a variable of type T. Returns the address of the pointer to said variable.
+	 */
+	template<typename T>
+	T *get_address(const char *name, const char *sig, int off);
 };
 
 template<typename T>
@@ -46,4 +53,32 @@ inline size_t Process::read_memory(T *address, T *out) {
 
 inline void Process::send_keypress(int key, bool down) {
 	debug("stub");
+}
+
+template<typename T>
+T *Process::get_address(const char *name, const char *sig, int off) {
+	auto address_ptr = reinterpret_cast<T **>(find_pattern(sig, off));
+
+	if (!address_ptr) {
+		// TODO: Replace this ASAP once std::format is a thing.
+		char buffer[128];
+		sprintf(buffer, "%s %s %s", "couldn't find the", name, "pointer");
+
+		throw std::runtime_error(buffer);
+	}
+
+	debug("%s %s %s %#x", "found", name, "pointer at", (uintptr_t)address_ptr);
+
+	T* address;
+	read_memory<T *>(address_ptr, &address);
+
+	if (!address) {
+		// TODO: See above.
+		char buffer[128];
+		sprintf(buffer, "%s %s %s", "found invalid", name, "pointer");
+
+		throw std::runtime_error(buffer);
+	}
+
+	return address;
 }
