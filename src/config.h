7f00e7ac9bd060acb2cac4ff3e5e770164c59dd6
Laurenz <root@fsoc.space>
2020-06-18 13:42:09 +0200

    Add proper defaults to humanization range

--- src/config.h
+++ src/config.h
@@ -22,9 +22,8 @@ namespace config {
 		options.add_options()
 			("h,help", "Show this message and exit.",
 				cxxopts::value(should_exit))
-			("humanization", "where arg is `a,b`. Add milliseconds in the range [a,b] to all key presses.",
-		    		cxxopts::value<std::vector<int>>(_humanization_range)
-		    			->default_value({ 0, 0 }));
+			("u,humanization", "where arg is `a,b`. Add milliseconds in the range [a,b] to all key presses. If only `a` is provided, defaults to [-a,a]. If nothing is provided, defaults to [0,0].",
+		    		cxxopts::value<std::vector<int>>(_humanization_range));
 
 		auto result = options.parse(argc, argv);
 
@@ -32,11 +31,15 @@ namespace config {
 			printf("%s\n", options.help().c_str());
 		}
 
-		if (_humanization_range.size() != 2) {
-			printf("--humanization expects two arguments separated by commas\n");
+		if (_humanization_range.empty()) {
+			humanization_range.first = 0;
+			humanization_range.second = 0;
+		} else if (_humanization_range.size() == 1) {
+			humanization_range.first = _humanization_range[0] * -1;
+			humanization_range.second = _humanization_range[0];
 		} else {
 			humanization_range.first = _humanization_range[0];
-			humanization_range.second = _humanization_range[1];
+			humanization_range.second =_humanization_range[1];
 		}
 	}
 }
