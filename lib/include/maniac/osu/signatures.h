05d3eb3c8b082b4e2d98fcad5e62d7287a3caafb
DayCheat <daycheat@protonmail.com>
2023-02-04 12:08:27 +0900

    Update signatures.h
    this should work

--- lib/include/maniac/osu/signatures.h
+++ lib/include/maniac/osu/signatures.h
@@ -9,8 +9,8 @@ namespace signatures {
 	constexpr auto STATE_SIG_OFFSET = 1;
 	constexpr auto STATE_SIG = "A1 ? ? ? ? A3 ? ? ? ? A1 ? ? ? ? A3 ? ? ? ? 83 3D ? ? ? ? 00 0F 84 ? ? ? ? B9 ? ? ? ? E8\0";
 
-	constexpr auto PLAYER_SIG_OFFSET = 3;
-	constexpr auto PLAYER_SIG = "33 D2 A1 ? ? ? ? 85 C0\0";
+	constexpr auto PLAYER_SIG_OFFSET = 1;
+	constexpr auto PLAYER_SIG = "A1 ? ? ? ? 8B ? ? ? 00 00 6A 00\0";
 
 	constexpr auto RULESET_SIG_OFFSET = 4;
 	constexpr auto RULESET_SIG = "73 7A 8B 0D ? ? ? ? 85 C9 74 1F\0";
